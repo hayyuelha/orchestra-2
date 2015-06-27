4a0c392f4d4b35d362a79f2cff4a86b69762f809
Muhammad Yafi <yafithekid212@gmail.com>
2015-06-27 21:54:39 +0700

    add highcharts

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,8 +7,8 @@
     <title>NAMA APLIKASI</title>
     
     <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-    <link href="{{asset('css/bootstrap.css')}}" rel='stylesheet' type='text/css' />
-    <link href="{{asset('css/style.css')}}" rel='stylesheet' type='text/css' />
+    <link href="{{asset('vendor/bootflat/css/bootstrap.min.css')}}" rel='stylesheet' type='text/css' />
+      <link href="{{asset('css/style.css')}}" rel='stylesheet' type='text/css' />
     <link href="{{asset('font-awesome/css/font-awesome.min.css')}}"/>
     <link href="{{asset('css/orchestra.css')}}" rel="stylesheet" type="text/css"/>
 
