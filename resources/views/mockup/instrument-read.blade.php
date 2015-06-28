f4c4d61115164198445dd1324e9a8aae078a6727
hayyuhanifah <heripottypot@gmail.com>
2015-06-29 04:34:18 +0700

    hapus legend dari map; tambah plain view untuk instrument-read dan music-read

--- resources/views/mockup/instrument-read.blade.php
+++ resources/views/mockup/instrument-read.blade.php
@@ -1,4 +1,36 @@
 @extends('layouts.master')
 @section('content')
-<h1>TODO instrument read</h1>
+<div class="col-sm-10 col-sm-offset-1">
+	<div class="grid_detail">
+		<h1>ANGKLUNG</h1>
+		<h3>Alat musik tradisional Jawa Barat</h3>
+		<hr/>
+		<h5>Diunggah oleh Afik | Suka </h5>
+		<hr/>
+		<div class="row">
+			<div class="col-sm-7">
+				<h1>WIKI</h1>
+				<p>Angklung adalah alat musik tradisional Jawa Barat yang lalalala lililili</p>
+			</div>
+			<div class="col-sm-3">
+				<div class="row">
+					<p>Demo</p>
+					<ul>
+						<li>A</li>
+						<li>C#</li>
+						<li>TODO: insert list of jPlayer here</li>
+					</ul>
+				</div>
+				<div class="row">
+					<p>Lagu yang dimainkan dengan alat ini:</p>
+					<ul>
+						<li>Lagu 1 #insert jPlayer here</li>
+						<li>Lagu 2 #insert jPlayer here</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<hr/>
+	</div>	
+</div>
 @endsection
--- resources/views/mockup/music-read.blade.php
+++ resources/views/mockup/music-read.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.master')
 @section('content')
-<h1>TODO music read</h1>
+<div class="col-sm-10 col-sm-offset-1">
+	<div class="grid_detail">
+		<h1>BUBUY BULAN</h1>
+		<h3>Lagu daerah Jawa Barat</h3>
+		<hr/>
+		<h3>TODO: Insert jPlayer plugin here</h3>
+		<h5>Diunggah oleh Afik | Suka | Tambah ke playlist</h5>
+		<hr/>
+		<div class="row">
+			<div class="col-sm-7">
+				<h1>WIKI</h1>
+				<p>Bubuy Bulan adalah lagu daerah Jawa Barat yang lalalala lililili</p>
+			</div>
+			<div class="col-sm-3">
+				<p>Lagu ini dapat dimainkan dengan:</p>
+				<ul>
+					<li>Angklung</li>
+					<li>Suling</li>
+				</ul>
+			</div>
+		</div>
+		<hr/>
+	</div>	
+</div>
 @endsection
