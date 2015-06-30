6219798a8996dc4cd9f19f5ec7f6dfa4ea116a5e
Muhammad Yafi <t-yafi@traveloka.com>
2015-06-30 18:35:05 +0700

    temporary commit, mau pulang

--- resources/views/mockup/music-create.blade.php
+++ resources/views/mockup/music-create.blade.php
@@ -1,4 +1,25 @@
 @extends('layouts.master')
 @section('content')
-<h1>TODO music create</h1>
+<div class="content_middle">
+    <div class="container">
+        <div class="content_middle_box_ed">
+            <div class="top_grid">
+            <div class="grid1 col-md-12">
+                <div class="inner_wrap" style="">
+                    <table class="music-box">
+                        <tr class="music-box-component">
+                            <td class="music-box-instrument">
+                                Instrument
+                            </td>
+                            <?php for($i = 0; $i < 1000; $i++): ?>
+                            <td>A</td>
+                            <?php endfor; ?>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
