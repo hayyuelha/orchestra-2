0a55197bbc619a0383f1e4a6913ea493be2b5877
hayyuelha <heripottypot@gmail.com>
2015-07-04 04:42:30 +0700

    temporary music-read

--- resources/views/mockup/music-read.blade.php
+++ resources/views/mockup/music-read.blade.php
@@ -5,12 +5,8 @@
    		<div class="content_middle_box_ed">
             <div class="top_grid">
    			    <div class="grid1 col-md-12">
-   				    <div class="view view-first">
+   				    <div class="view">
                         <div class="index_img_orgbottom"><img src="/images/angklung1.jpg" class="img-responsive" alt=""/></div>
-   				        <div class="mask" href="#">
-                        	<p> Bubuy Bulan adalah lagu dari Jawa Barat. Lalalala lalalalalalala </p>
-                        	<div class="info"><i class="search"> </i> Show More</div>
-                        </div>
                     </div> 
                     <i class="songicon"></i>
    				    <div class="inner_wrap">
@@ -20,54 +16,74 @@
 		                    <a href="#"> <span class="fa fa-code-fork"> fork</a> | 
 		                    <a href="#"> <span class="fa fa-plus"> add to playlist</a>
 		                </p>
-		                <div id="jquery_jplayer_1" class="jp-jplayer"></div>
-							<div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
-							  <div class="jp-type-single">
-							    <div class="jp-gui jp-interface">
-							      <div class="jp-volume-controls">
-							        <button class="jp-mute" role="button" tabindex="0">mute</button>
-							        <button class="jp-volume-max" role="button" tabindex="0">max volume</button>
-							        <div class="jp-volume-bar">
-							          <div class="jp-volume-bar-value"></div>
-							        </div>
-							      </div>
-							      <div class="jp-controls-holder">
-							        <div class="jp-controls">
-							          <button class="jp-play" role="button" tabindex="0">play</button>
-							          <button class="jp-stop" role="button" tabindex="0">stop</button>
-							        </div>
-							        <div class="jp-progress">
-							          <div class="jp-seek-bar">
-							            <div class="jp-play-bar"></div>
-							          </div>
-							        </div>
-							        <div class="jp-current-time" role="timer" aria-label="time">&nbsp;</div>
-							        <div class="jp-duration" role="timer" aria-label="duration">&nbsp;</div>
-							        <div class="jp-toggles">
-							          <button class="jp-repeat" role="button" tabindex="0">repeat</button>
-							        </div>
-							      </div>
-							    </div>
-							    <div class="jp-details">
-							      <div class="jp-title" aria-label="title">&nbsp;</div>
-							    </div>
-							    <div class="jp-no-solution">
-							      <span>Update Required</span>
-							      To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
-							    </div>
-							  </div>
-							</div>
 	   				    <div class="row">
-							<div class="col-sm-7">
-								<h1>WIKI</h1>
-								<p>Bubuy Bulan adalah lagu daerah Jawa Barat yang lalalala lililili</p>
+							<div class="col-sm-7 wiki-music">
+								<p>Bubuy Bulan adalah lagu daerah Jawa Barat yang diciptakan oleh Benny Korda. Adapun lirik lagu Bubuy Bulan adalah sebagai berikut:</p>
+								<blockquote>
+									Bubuy bulan</br>
+									Bubuy bulan sangray bentang</br>
+									Panon poe</br>
+									Panon poe disasate</br>
+									</br>
+									Unggal bulan</br>
+									Unggal bulan abdi teang</br>
+									Unggal poe</br>
+									Unggal poe oge hade</br>
+									</br>
+									Situ Ciburuy</br>
+									laukna hese dipancing</br>
+									Nyeredet hate</br>
+									Ningali ngeplak caina</br>
+									</br>
+									Duh eta saha</br>
+									Nu ngalangkung unggal enjing</br>
+									Nyeredet hate</br>
+									Ningali sorot socana</br>
+								</blockquote>
 							</div>
 							<div class="col-sm-3">
-								<p>Lagu ini dapat dimainkan dengan:</p>
-								<ul>
-									<li>Angklung</li>
-									<li>Suling</li>
-								</ul>
+								<div class="row">
+									<div id="jquery_jplayer_1" class="jp-jplayer"></div>
+									<div id="jp_container_1" class="jp-audio">
+									  <div class="jp-type-single">
+									 
+									    <div class="jp-gui jp-interface">
+									 
+									        <ul class="jp-controls">
+									          <li><a href="javascript:;" class="jp-play" tabindex="1">&#61515;</a></li>
+									          <li><a href="javascript:;" class="jp-pause" tabindex="1">&#61516;</a></li>
+									          <li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">&#61480;</a></li>
+									          <li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">&#61478;</a></li>
+									        </ul>
+									 
+									        <div class="jp-progress">
+									          <div class="jp-seek-bar">
+									            <div class="jp-play-bar"></div>
+									          </div>
+									        </div>
+									 
+									        <div class="jp-time-holder">
+									          <div class="jp-current-time"></div>
+									        </div>
+									 
+									        <div class="jp-volume-bar">
+									          <div class="jp-volume-bar-value"></div>
+									        </div>
+									 
+									    <div class="jp-no-solution">
+									      <span>Update Required</span>
+									      To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
+									    </div>
+									  </div>
+									</div>
+								<div class="row played-with">
+									<p>Lagu ini dapat dimainkan dengan:</p>
+									<ul>
+										<li>Angklung</li>
+										<li>Suling</li>
+									</ul>
+								</div>
+								
 							</div>
 						</div>
    				    </div>
@@ -82,4 +98,23 @@
 @section('script')
 	@parent
     <script src="{{asset('js/player/jplayer/dist/jplayer/jquery.jplayer.js')}}"></script>
+    <script type="text/javascript">
+	    $(document).ready(function(){	
+	      $("#jquery_jplayer_1").jPlayer({
+	        ready: function() {
+	          $(this).jPlayer("setMedia", {
+	            mp3: "{{asset('music/kicir-kicir.mp3')}}"
+	          }).jPlayer("play");
+	          var click = document.ontouchstart === undefined ? 'click' : 'touchstart';
+	          var kickoff = function () {
+	            $("#jquery_jplayer_1").jPlayer("play");
+	            document.documentElement.removeEventListener(click, kickoff, true);
+	          };
+	          document.documentElement.addEventListener(click, kickoff, true);
+	        },
+	        swfPath: "/js",
+	        loop: false
+	      });
+	    });
+	  </script>
 @endsection
